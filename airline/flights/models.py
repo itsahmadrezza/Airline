26ccc3ce95f171cca1280be66790da4a649dabda
itsahmadrezza <Ahmadreza.Lashkary.Rad@gmail.com>
2022-06-11 07:39:03 -0700

    Crate view index

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Airport(models.Model):
+    code = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+    
+
+    
+class Flight(models.Model):
+    origin = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="departures")    
+    detination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="arrivals")    
+    duration = models.IntegerField()
